559644d8e12bfb83bfef79ed692c07ae1eab3939
AJ <735267452@qq.com>
2015-11-06 09:51:29 +0800

    select words

--- EnglistWords/query.php
+++ EnglistWords/query.php
@@ -6,12 +6,22 @@ class Query {
     public $url = "http://dict.youdao.com/fsearch?client=deskdict&keyfrom=chrome.extension&pos=-1&doctype=xml&xmlVersion=3.2&dogVersion=1.0&vendor=unknown&appVer=3.1.17.4208&le=eng&q=";
     public function ajax () {
         $w = new Word();
-            $w->get(785378, 1000);
+        $w->get(1, 1000);
     }
+    public function select () {
+        $w = new Word();
+        $rows = $w->select(100, 10);
+//        foreach($rows as $key=>$val) {
+//            echo $val["w_desc"]."<hr>";
+//        }
+        echo json_encode($rows);
+    }
+
 }
 
 set_time_limit(0);
 $q = new Query();
-$q->ajax();
+//$q->ajax();
+$q->select();
 
 ?>
