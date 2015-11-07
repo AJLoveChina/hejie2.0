cefaa34038595e10e642aec68e6473db799ba58a
AJ <735267452@qq.com>
2015-11-07 18:13:28 +0800

    ...

---
+++ gists/gists.js
@@ -0,0 +1,9 @@
+(function () {
+
+    // var url = "https://api.github.com/gists/62eff078601225c94726";
+
+    // 1.List gists
+    // var url = "https://api.github.com/users/ajlovechina";
+    // $.get("https://api.github.com/users/ajlovechina");
+
+})();
