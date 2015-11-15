3f34c7ff58922e0e347d4140319fe371eac0c011
AJ <735267452@qq.com>
2015-11-15 15:23:53 +0800

    ..

--- ajaxSeo/demo.js
+++ ajaxSeo/demo.js
@@ -2,10 +2,21 @@
  * Created by ajax on 2015/11/13.
  */
 
-(function () {
-    console.log(1);
+
+(function(window,undefined){
+
+    // Bind to StateChange Event
+    History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+        var State = History.getState(); // Note: We are using History.getState() instead of event.state
+        console.log(State);
+    });
+
+    //History.pushState({state:1}, "State 1", "?state=1"); // logs {state:1}, "State 1", "?state=1"
+
+    History.pushState({state:2}, "Page 2","?page=2");
+
     $('#container').on('click', 'a', function (e) {
         e.preventDefault();
-        window.history.pushState(null, null, $(this).attr('href'));
+        History.pushState(null, null, $(this).attr("href"));
     });
-})();
+})(window);
