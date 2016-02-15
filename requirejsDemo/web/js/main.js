6cfcd69b2e4386d2d0c9546c816979cb20f2002a
AJ <735267452@qq.com>
2016-02-15 20:36:57 +0800

    requirejs

--- requirejsDemo/web/js/main.js
+++ requirejsDemo/web/js/main.js
@@ -1,7 +1,23 @@
-/**
- * Created by ajax on 2016/1/14.
- */
-requirejs(["helper/util"], function(util) {
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'js/',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        app: '../app',
+        con : "controller/"
+    }
+});
+
+// con not work
+//requirejs(['con/X.js'], function (X, Y) {
+//
+//});
 
 
+requirejs(["js/model/A.js"], function (A) {
+    console.log(A);
 });
