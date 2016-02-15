586cf1e7fb9fbaf85d421ae564c47ee2e3f4e8ac
AJ <735267452@qq.com>
2016-02-15 21:34:26 +0800

    ...

--- requirejsDemo/web/js/controller/do1.js
+++ requirejsDemo/web/js/controller/do1.js
@@ -1,7 +1,6 @@
 define(function (require, exports, module) {
-    var C = require("../model/C.js");
+    var C = require("js/model/C.js");
 
     var c1 = new C();
     c1.main();
-
 });
--- requirejsDemo/web/js/main.js
+++ requirejsDemo/web/js/main.js
@@ -1,17 +1,9 @@
-requirejs.config({
-    //By default load any module IDs from js/lib
-    baseUrl: 'js/',
-    //except, if the module ID starts with "app",
-    //load it from the js/app directory. paths
-    //config is relative to the baseUrl, and
-    //never includes a ".js" extension since
-    //the paths config could be for a directory.
+require.config({
+    baseUrl: "js",
     paths: {
-        app: '../app',
-        con : "controller/"
+        "do1": "controller/do1"
     }
 });
-
 // con not work
 //requirejs(['con/X.js'], function (X, Y) {
 //
