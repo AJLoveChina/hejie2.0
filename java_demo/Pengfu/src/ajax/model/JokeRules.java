2506bcfc360437b96228630f2ba2f096f0b9315d
AJ <735267452@qq.com>
2016-03-05 23:49:39 +0800

    selector

---
+++ java_demo/Pengfu/src/ajax/model/JokeRules.java
@@ -0,0 +1,10 @@
+package ajax.model;
+
+public class JokeRules {
+	private String tableName = "joke";
+	private String url;
+	private String titleSelector;
+	private String contentSelector;
+	
+	
+}
