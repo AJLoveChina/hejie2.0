1fa134e9fe8870de0b82398d9533346ebe433977
AJ <735267452@qq.com>
2016-04-01 12:22:00 +0800

    ...

--- java_demo3/spider/src/main/java/ajax/crawler/TiebaCrawler.java
+++ java_demo3/spider/src/main/java/ajax/crawler/TiebaCrawler.java
@@ -3,8 +3,13 @@ package ajax.crawler;
 import cn.edu.hfut.dmic.webcollector.crawler.BreadthCrawler;
 import cn.edu.hfut.dmic.webcollector.model.Links;
 import cn.edu.hfut.dmic.webcollector.model.Page;
+
+import java.io.IOException;
 import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import ajax.model.entity.Tieba;
@@ -26,38 +31,59 @@ public class TiebaCrawler extends BreadthCrawler{
     @Override
     public void visit(Page page, Links nextLinks) {
         String url = page.getUrl();
+        
+        String huifu = page.getDoc().select("#thread_theme_5 > div.l_thread_info > ul > li:nth-child(2) > span:nth-child(1)").text();
+        int huifu2;
+        try {
+        	huifu2 = Integer.parseInt(huifu);
+        }catch(Exception e) {
+        	huifu2 = 0;
+        }
+        
+        if (huifu2 < 200) {
+        	return;
+        }
+      
         /*if page is news page*/
         if (Pattern.matches("http://tieba.baidu.com/p/\\d+", url)) {
            
-            Document doc = page.getDoc();
-
-            /*extract title and content of news by css selector*/
-            Elements contents = doc.select(".d_post_content");
-            Elements title = doc.select("#j_core_title_wrap > h3");
-            
-            
-            Tieba tb = new Tieba();
-            tb.setContent(contents.html());
-            tb.setTitle(title.html());
-            
-            tb.save();
-            
-            
-
-            /*If you want to add urls to crawl,add them to nextLinks*/
-            /*WebCollector automatically filters links that have been fetched before*/
-            /*If autoParse is true and the link you add to nextLinks does not match the regex rules,the link will also been filtered.*/
-            // nextLinks.add("http://xxxxxx.com");
+            Document doc;
+			try {
+				String trueUrl = url + "?see_lz=1";
+				doc = Jsoup.connect(trueUrl).get();
+				
+				 /*extract title and content of news by css selector*/
+	            Elements contents = doc.select(".d_post_content");
+	            Elements title = doc.select("#j_core_title_wrap > h3");
+	            Elements username = doc.select("#j_p_postlist > div:nth-child(1) > div.d_author > ul > li.d_name > a");
+	            
+	            Tieba tb = new Tieba();
+	            StringBuilder content = new StringBuilder();
+	            for (Element ele : contents) {
+	            	content.append(ele.html());
+	            	content.append("<br>");
+	            }
+	            tb.setContent(content.toString());
+	            tb.setTitle(title.html());
+	            tb.setLikes(huifu2);
+	            tb.setUsername(username.text());
+	            tb.setUrl(trueUrl);
+	            
+	            tb.save();
+	            
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
         }
     }
 
     public static void main(String[] args) throws Exception {
         TiebaCrawler crawler = new TiebaCrawler("crawl", true);
-        crawler.setThreads(1);
-        crawler.setTopN(100);
+        crawler.setThreads(50);
+        crawler.setTopN(10000);
         //crawler.setResumable(true);
         /*start crawl with depth of 4*/
-        crawler.start(4);
+        crawler.start(20);
     }
 
 }
