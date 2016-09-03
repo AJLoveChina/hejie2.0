a881872ec117bee3680f64177f23df35be0cadc4
AJ <735267452@qq.com>
2016-09-03 21:30:28 +0800

    fix project problem

---
+++ neon/hyy/src/main/java/ajax/controller/spring/TaskController.java
@@ -0,0 +1,26 @@
+package ajax.controller.spring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+	
+	
+	@RequestMapping("/list")
+	public List<String> getTasks() {
+		List<String> list = new ArrayList<String>();
+		
+		
+		list.add("Jack");
+		list.add("Lily");
+		list.add("Bob");
+		
+		return list;	
+	}
+	
+}
