518b9f10c760b7980c5c9e2914356618ebc5d7cf
AJ <735267452@qq.com>
2016-09-22 22:15:11 +0800

    build-require-modules.bat

---
+++ neon/hyy/src/main/webapp/WEB-INF/build-require-modules.bat
@@ -0,0 +1,6 @@
+@echo off
+G:
+cd G:\XAMPP\htdocs\Github\meAjax\neon\hyy\src\main\webapp\web\js\modules
+node r.js -o requirejs-build.js
+echo "r.js打包完成!"
+pause
--- neon/hyy/src/main/webapp/web/js/modules/ui/comment.js
+++ neon/hyy/src/main/webapp/web/js/modules/ui/comment.js
@@ -8,8 +8,10 @@ define(function () {
 				return jDom.attr(this.groupIdAttr);
 			},
 			render : function () {
-				console.log("I am rendering comments ui...");
-				console.log("I am rendering comments ui2...");
+				console.log("I am rendering comments ui7...");
+				console.log("I am rendering comments ui8...");
+				console.log("I am rendering comments ui9...");
+				console.log("I am rendering comments ui10...");
 			}
 	}
 	
