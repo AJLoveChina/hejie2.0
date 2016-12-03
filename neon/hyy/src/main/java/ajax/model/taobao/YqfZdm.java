bac4bdb9bb6cff39569c388e8373a64c9e8e9f8f
AJ <735267452@qq.com>
2016-12-03 22:37:53 +0800

    yiqifa zdm

---
+++ neon/hyy/src/main/java/ajax/model/taobao/YqfZdm.java
@@ -0,0 +1,258 @@
+package ajax.model.taobao;
+
+public class YqfZdm {
+	private Long id;
+	private String title;
+	private String subTitle;
+	private int channel;
+	private int status;
+	private String recommendedReason;
+	private String shareContent;
+	private String couponCode;
+	private String tag;
+	private String picUrl;
+	private int weight;
+	private String publishTime;
+	private String updateTime;
+	private String shopName;
+	private String shopId;
+	private String productName;
+	private String pc_productYiqifaUrl;
+	private String moblie_productYiqifaUrl;
+	private double pc_discountPrice;
+	private double moblie_discountPrice;
+	private double pc_reservePrice;
+	private double moblie_reservePrice;
+	private String brands;
+	private String storeName;
+	private boolean isPost;
+	private long volume;
+	private String Category1Id;
+	private String Category1Name;
+	private String Category2Id;
+	private String Category2Name;
+	private int pc_comissionRate;
+	private int moblie_comissionRate;
+	private String startTime;
+	private String endTime;
+	private int productStatus;
+	private String region;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSubTitle() {
+		return subTitle;
+	}
+	public void setSubTitle(String subTitle) {
+		this.subTitle = subTitle;
+	}
+	public int getChannel() {
+		return channel;
+	}
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getRecommendedReason() {
+		return recommendedReason;
+	}
+	public void setRecommendedReason(String recommendedReason) {
+		this.recommendedReason = recommendedReason;
+	}
+	public String getShareContent() {
+		return shareContent;
+	}
+	public void setShareContent(String shareContent) {
+		this.shareContent = shareContent;
+	}
+	public String getCouponCode() {
+		return couponCode;
+	}
+	public void setCouponCode(String couponCode) {
+		this.couponCode = couponCode;
+	}
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public String getPicUrl() {
+		return picUrl;
+	}
+	public void setPicUrl(String picUrl) {
+		this.picUrl = picUrl;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public String getPublishTime() {
+		return publishTime;
+	}
+	public void setPublishTime(String publishTime) {
+		this.publishTime = publishTime;
+	}
+	public String getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public String getShopId() {
+		return shopId;
+	}
+	public void setShopId(String shopId) {
+		this.shopId = shopId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getPc_productYiqifaUrl() {
+		return pc_productYiqifaUrl;
+	}
+	public void setPc_productYiqifaUrl(String pc_productYiqifaUrl) {
+		this.pc_productYiqifaUrl = pc_productYiqifaUrl;
+	}
+	public String getMoblie_productYiqifaUrl() {
+		return moblie_productYiqifaUrl;
+	}
+	public void setMoblie_productYiqifaUrl(String moblie_productYiqifaUrl) {
+		this.moblie_productYiqifaUrl = moblie_productYiqifaUrl;
+	}
+	public double getPc_discountPrice() {
+		return pc_discountPrice;
+	}
+	public void setPc_discountPrice(double pc_discountPrice) {
+		this.pc_discountPrice = pc_discountPrice;
+	}
+	public double getMoblie_discountPrice() {
+		return moblie_discountPrice;
+	}
+	public void setMoblie_discountPrice(double moblie_discountPrice) {
+		this.moblie_discountPrice = moblie_discountPrice;
+	}
+	public double getPc_reservePrice() {
+		return pc_reservePrice;
+	}
+	public void setPc_reservePrice(double pc_reservePrice) {
+		this.pc_reservePrice = pc_reservePrice;
+	}
+	public double getMoblie_reservePrice() {
+		return moblie_reservePrice;
+	}
+	public void setMoblie_reservePrice(double moblie_reservePrice) {
+		this.moblie_reservePrice = moblie_reservePrice;
+	}
+	public String getBrands() {
+		return brands;
+	}
+	public void setBrands(String brands) {
+		this.brands = brands;
+	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public boolean isPost() {
+		return isPost;
+	}
+	public void setPost(boolean isPost) {
+		this.isPost = isPost;
+	}
+	public long getVolume() {
+		return volume;
+	}
+	public void setVolume(long volume) {
+		this.volume = volume;
+	}
+	public String getCategory1Id() {
+		return Category1Id;
+	}
+	public void setCategory1Id(String category1Id) {
+		Category1Id = category1Id;
+	}
+	public String getCategory1Name() {
+		return Category1Name;
+	}
+	public void setCategory1Name(String category1Name) {
+		Category1Name = category1Name;
+	}
+	public String getCategory2Id() {
+		return Category2Id;
+	}
+	public void setCategory2Id(String category2Id) {
+		Category2Id = category2Id;
+	}
+	public String getCategory2Name() {
+		return Category2Name;
+	}
+	public void setCategory2Name(String category2Name) {
+		Category2Name = category2Name;
+	}
+	public int getPc_comissionRate() {
+		return pc_comissionRate;
+	}
+	public void setPc_comissionRate(int pc_comissionRate) {
+		this.pc_comissionRate = pc_comissionRate;
+	}
+	public int getMoblie_comissionRate() {
+		return moblie_comissionRate;
+	}
+	public void setMoblie_comissionRate(int moblie_comissionRate) {
+		this.moblie_comissionRate = moblie_comissionRate;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public int getProductStatus() {
+		return productStatus;
+	}
+	public void setProductStatus(int productStatus) {
+		this.productStatus = productStatus;
+	}
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	
+	
+}
