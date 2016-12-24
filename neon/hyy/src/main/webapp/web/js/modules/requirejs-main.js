8000c618d10c79ebe86c5ed6b002694d95168d1e
AJ <735267452@qq.com>
2016-12-24 23:52:41 +0800

    christmas

--- neon/hyy/src/main/webapp/web/js/modules/requirejs-main.js
+++ neon/hyy/src/main/webapp/web/js/modules/requirejs-main.js
@@ -1,3 +1,3 @@
-require(["tools/customEvent", "ui/comment", "model/user", "tools/tools", "tools/seo", "tools/googleCustomSearch", "model/Goods"], function () {
+require(["tools/customEvent", "ui/comment", "model/user", "tools/tools", "tools/seo",  "model/Goods"], function () {
 	
 });
